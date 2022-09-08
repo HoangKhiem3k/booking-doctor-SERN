2a0c7bf4b5f101c3dc7319b1a114984d269afacd
HoangKhiem3k <ledohoangkhiem3k@gmail.com>
2022-09-08 08:40:39 +0700

    en-vi-admin

--- frontend/src/containers/Header/Header.js
+++ frontend/src/containers/Header/Header.js
@@ -1,43 +1,68 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import * as actions from "../../store/actions";
-import Navigator from '../../components/Navigator';
-import { adminMenu } from './menuApp';
-import './Header.scss';
-
+import Navigator from "../../components/Navigator";
+import { adminMenu } from "./menuApp";
+import "./Header.scss";
+import { LANGUAGES } from "../../utils";
 class Header extends Component {
+  handleChangeLanguage = (language) => {
+    this.props.changeLanguageAppRedux(language);
+  };
+  render() {
+    const { processLogout, language } = this.props;
 
-    render() {
-        const { processLogout } = this.props;
-
-        return (
-            <div className="header-container">
-                {/* thanh navigator */}
-                <div className="header-tabs-container">
-                    <Navigator menus={adminMenu} />
-                </div>
-
-                {/* nút logout */}
-                <div className="btn btn-logout" onClick={processLogout}>
-                    <i className="fas fa-sign-out-alt"></i>
-                </div>
-            </div>
-        );
-    }
-
+    return (
+      <div className="header-container">
+        {/* thanh navigator */}
+        <div className="header-tabs-container">
+          <Navigator menus={adminMenu} />
+        </div>
+        <div className="languages">
+          <span
+            className={
+              language === LANGUAGES.VI ? "active language-vi" : "language-vi"
+            }
+            onClick={() => {
+              this.handleChangeLanguage(LANGUAGES.VI);
+            }}
+          >
+            VN
+          </span>
+          <span
+            className={
+              language === LANGUAGES.EN ? "active language-en" : "language-en"
+            }
+            onClick={() => {
+              this.handleChangeLanguage(LANGUAGES.EN);
+            }}
+          >
+            EN
+          </span>
+          {/* nút logout */}
+          <div className="btn btn-logout" onClick={processLogout}>
+            <i className="fas fa-sign-out-alt"></i>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.user.isLoggedIn
-    };
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        processLogout: () => dispatch(actions.processLogout()),
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    processLogout: () => dispatch(actions.processLogout()),
+    changeLanguageAppRedux: (language) =>
+      dispatch(actions.changeLanguageApp(language)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
