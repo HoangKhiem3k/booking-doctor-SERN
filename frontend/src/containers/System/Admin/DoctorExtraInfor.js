0a218a2830bdf0237244791a27036e59d3d32db4
HoangKhiem3k <ledohoangkhiem3k@gmail.com>
2022-09-25 07:50:37 +0700

    detail-specialty-1

--- frontend/src/containers/System/Admin/DoctorExtraInfor.js
+++ frontend/src/containers/System/Admin/DoctorExtraInfor.js
@@ -13,7 +13,14 @@ class DoctorExtraInfor extends Component {
       extraInfor: {},
     };
   }
-  componentDidMount() {}
+  async componentDidMount() {
+    if (this.props.doctorIdFromParent) {
+      let res = await getExtraInforDoctorById(this.props.doctorIdFromParent);
+      if (res && res.errCode === 0) {
+        this.setState({ extraInfor: res.data });
+      }
+    }
+  }
   async componentDidUpdate(prevProps, prevState) {
     if (this.props.language !== prevProps.language) {
     }
--- frontend/src/containers/System/Specialty/DetailSpecialty.js
+++ frontend/src/containers/System/Specialty/DetailSpecialty.js
@@ -2,11 +2,17 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { FormattedMessage } from "react-intl";
 import { LANGUAGES } from "../../../utils/constant";
-// import HomeHeader from ".
+import HomeHeader from "../../HomePage/HomeHeader";
+import DoctorSchedule from "../../Patient/Doctor/DoctorSchedule";
+import DoctorExtraInfor from "../Admin/DoctorExtraInfor";
+import ProfileDoctor from "../../Patient/Doctor/ProfileDoctor";
+import "./DetailSpecialty.scss";
 class DetailSpecialty extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      arrDoctorId: [10, 11, 12],
+    };
   }
   componentDidMount() {}
   componentDidUpdate(prevProps, prevState) {
@@ -15,10 +21,38 @@ class DetailSpecialty extends Component {
   }
 
   render() {
+    let { arrDoctorId } = this.state;
     return (
-      <>
-        <div>default spec</div>;
-      </>
+      <div className="detail-specialty-container">
+        <HomeHeader />
+        <div className="detail-specialty-body">
+          <div className="description-specialty"></div>
+          {arrDoctorId &&
+            arrDoctorId.length > 0 &&
+            arrDoctorId.map((item, index) => {
+              return (
+                <div className="each-doctor" key={index}>
+                  <div className="dt-content-left">
+                    <div className="profile-doctor">
+                      <ProfileDoctor
+                        doctorId={item}
+                        isShowDescriptionDoctor={true}
+                      />
+                    </div>
+                  </div>
+                  <div className="dt-content-right">
+                    <div className="doctor-schedule">
+                      <DoctorSchedule doctorIdFromParent={item} />
+                    </div>
+                    <div className="doctor-extra-infor">
+                      <DoctorExtraInfor doctorIdFromParent={item} />
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     );
   }
 }
